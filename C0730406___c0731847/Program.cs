464683b760a9d5bbe5e56fe98c1688f7110f1b4c
visualstudio <visualstudio@122-PC12>
2019-03-15 20:49:28 -0400

    Add project files.

---
+++ C0730406___c0731847/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0730406___c0731847
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
